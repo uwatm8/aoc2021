26225ab4e634bfe2bb065437fda56fc905e15988
Arvid Hast <arvid.hast@polestar.com>
2021-12-03 07:00:10 +0100

    day 3 working

---
+++ 3/1.py
@@ -0,0 +1,51 @@
+filepath = 'data'
+dataSuffix = 1
+ls = []
+with open(filepath+str(dataSuffix)) as fp:
+    line = fp.readline()
+    while line:
+        ls.append(line.strip())
+        line = fp.readline()
+na = len(ls)
+
+ans = "0b"
+
+l = {}
+
+print(":(", l)
+
+for i, a in enumerate(ls):
+    for i2, c in enumerate(a):
+        key = c+'-'+str(i2)
+
+        if not key in l:
+            l[key] = 1
+            print(l)
+
+        else:
+            l[key] += 1
+            print(l)
+    print("hej", l)
+
+
+for i in range(12):
+    n0 = 0
+    n1 = 0
+
+    if('0-'+str(i) in l):
+        n0 = l['0-'+str(i)]
+        print("n0", n0)
+
+    if('1-'+str(i) in l):
+        n1 = l['1-'+str(i)]
+        print("n1", n1)
+
+    print(n0, n1)
+
+    if n0 > n1:
+        ans += '0'
+    else:
+        ans += '1'
+
+
+print("answer: ", ans)
---
+++ 3/2.py
@@ -0,0 +1,84 @@
+filepath = 'data'
+dataSuffix = 2
+ls = []
+with open(filepath+str(dataSuffix)) as fp:
+    line = fp.readline()
+    while line:
+        ls.append(line.strip())
+        line = fp.readline()
+na = len(ls)
+
+ols = ls
+
+ans = "0b"
+
+l = {}
+
+
+prefix = ""
+n1 = 0
+n0 = 0
+
+for j in range(len(ls[0])):
+    n1 = 0
+    n0 = 0
+
+    a0 = []
+    a1 = []
+
+    for a in ls:
+        if a.startswith(prefix+'0'):
+            n0 += 1
+            a0.append(a)
+        else:
+            n1 += 1
+            a1.append(a)
+
+    if n1 >= n0:
+        prefix += '1'
+        ls = a1
+    else:
+        prefix += '0'
+        ls = a0
+
+print(prefix)
+
+f1 = prefix
+
+ls = ols
+
+prefix = ""
+
+for j in range(len(ls[0])):
+    n1 = 0
+    n0 = 0
+
+    a0 = []
+    a1 = []
+
+    for a in ls:
+        if a.startswith(prefix+'0'):
+            n0 += 1
+            a0.append(a)
+        else:
+            n1 += 1
+            a1.append(a)
+
+    if len(ls) != 1:
+        if n1 < n0:
+            prefix += '1'
+            ls = a1
+        else:
+            prefix += '0'
+            ls = a0
+    else:
+        prefix += ls[0][j]
+
+    print(ls, n0, n1)
+
+print(prefix)
+
+f2 = prefix
+
+
+print("answer: ", f1, f2)
